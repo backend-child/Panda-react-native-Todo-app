65393f2ee30008b4f0e916fa3476f7e501a44f24
backend-child <ejikeanaz66@gmail.com>
2023-07-19 15:53:15 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useState } from "react";
+import Header from "./component/Header";
+import TodoItem from "./component/TodoItem";
+import AddTodo from "./component/AddTodo";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "Check Daily Time Frame", key: "1" },
+    { text: "Plan My Structure", key: "2" },
+    { text: "Check My LIquidity Pools", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodo) => {
+      return prevTodo.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodo) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodo];
+      });
+    } else {
+      Alert.alert("OOPS!", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed !") },
+      ]);
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+        </View>
+
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+
+  content: {
+    padding: 40,
+  },
+
+  list: {
+    marginTop: 20,
+  },
+});
